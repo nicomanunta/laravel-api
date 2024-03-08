034f9bdc3217ef52e5366ab3f3808d2382d22db6
nicomanunta <nic.manun@gmail.com>
2024-03-08 15:13:06 +0100

    importato controller e aggiunta rotta

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -24,7 +24,7 @@ class ProjectController extends Controller
         $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
 
         return response()->json([
-            'succes' => true,
+            'success' => true,
             'project' => $project,
         ]);
     }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController as ProjectController;
+use App\Http\Controllers\Api\LeadController as LeadController;
 
 
 /*
@@ -17,7 +18,9 @@ use App\Http\Controllers\Api\ProjectController as ProjectController;
 */
 
 Route::get('/projects', [ProjectController::class, 'index']);
-Route::get('/projects/{slug}', [ProjectController::class, 'shoe']);
+Route::get('/projects/{slug}', [ProjectController::class, 'show']);
+Route::get('/contacts', [LeadController::class, 'store']);
+
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
