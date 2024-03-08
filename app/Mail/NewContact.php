3e542fae03a9470cfd208ece6e5e109b547dd1f4
nicomanunta <nic.manun@gmail.com>
2024-03-08 14:18:18 +0100

    aggiunte funzioni e creato contact-email

--- app/Mail/NewContact.php
+++ app/Mail/NewContact.php
@@ -13,14 +13,16 @@ class NewContact extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $lead;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($_lead)
     {
-        //
+        $this->lead = $_lead;
     }
 
     /**
@@ -31,7 +33,8 @@ class NewContact extends Mailable
     public function envelope()
     {
         return new Envelope(
-            subject: 'New Contact',
+            subject: 'Nuovo contatto',
+            replyTo: $this->lead->address,
         );
     }
 
@@ -43,7 +46,7 @@ class NewContact extends Mailable
     public function content()
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.contact-email',
         );
     }
 
---
+++ resources/views/emails/contact-email.blade.php
@@ -0,0 +1,8 @@
+<h1>Ciao Admin</h1>
+<p>
+    hai ricevuto una nuova mail: <br>
+    Nome:{{$lead->name}} <br>
+    Cognome: {{$lead->surname}} <br>
+    Email: {{$lead->email}} <br>
+    Messaggio: {{$lead->message}} <br>
+</p>
