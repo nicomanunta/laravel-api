4ba3479ce3b3bc4c11008c782967bbeb5fa036d4
nicomanunta <nic.manun@gmail.com>
2024-03-08 14:33:44 +0100

    nuovo controller l'altro era sbagliato, migrate

--- database/migrations/2024_03_08_132344_create_leads_table.php
+++ database/migrations/2024_03_08_132344_create_leads_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('leads', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->string('surname', 50);
+            $table->string('email', 100);
+            $table->string('phone', 20);
+            $table->text('message');
             $table->timestamps();
         });
     }
